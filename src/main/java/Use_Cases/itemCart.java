cc53b120797cd40089f066963d0f268ae7acecf0
Janel Gilani <gilani.janel@gmail.com>
2022-11-09 13:43:09 -0500

    created functionality to move items into item cart and then to order.

--- src/main/java/Use_Cases/itemCart.java
+++ src/main/java/Use_Cases/itemCart.java
@@ -1,5 +1,24 @@
 package Use_Cases;
 
+import Entities.Order;
+import Entities.foodItem;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class itemCart {
+    public ArrayList<foodItem> currentOrder;
+
+    public Order makeOrder () {
+        LocalDateTime dateTime = LocalDateTime.now();
+        Order tempOrder = new Order(dateTime);
+
+        for (foodItem item: this.currentOrder) {
+            item.addToOrder(tempOrder);
+        }
+
+        this.currentOrder.clear();
+        return tempOrder;
+    }
 
 }
