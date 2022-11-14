b5b388b6f6e57a38c6483402bf8e550d4b303280
Akshayan03 <akshayan.jeyakumar@mail.utoronto.ca>
2022-11-14 14:55:49 -0500

    finished editing the problems with the Budget Entity and cleaned up the Budget Manager use case  by editing and finishing off the methods, one more method left to do, but need to commit and push in order to pull new method from pastOrders to use

--- src/main/java/Entities/Budget.java
+++ src/main/java/Entities/Budget.java
@@ -1,16 +1,37 @@
 package Entities;
 
 public class Budget {
-    private double initialBudget;
-    private double currentBudget;
+    public double initialBudget;
+    public double currentBudget;
+
+    public Budget () {
+        this.initialBudget = 0;
+        this.currentBudget = 0;
+    }
 
     public Budget (double initialBudget, double currentBudget){
         this.initialBudget = initialBudget;
         this.currentBudget = currentBudget;
     }
 
-    public Budget () {
-        this.initialBudget = 0;
-        this.currentBudget = 0;
+    public double getInitialBudget() {
+        return initialBudget;
+    }
+
+    public void setInitialBudget(double initialBudget) {
+        if (initialBudget < 0)
+            throw new IllegalArgumentException("Initial Budget cannot be less than 0");
+        this.initialBudget = initialBudget;
+    }
+
+    public double getCurrentBudget() {
+        return currentBudget;
+    }
+
+    public void setCurrentBudget(double currentBudget) {
+        if (currentBudget < 0)
+            throw new IllegalArgumentException("Current Budget cannot be less than 0");
+        this.currentBudget = currentBudget;
+
     }
 }
---
+++ src/main/java/Use_Cases/budgetManager.java
@@ -0,0 +1,36 @@
+package Use_Cases;
+
+import Entities.Budget;
+
+import java.util.Scanner;
+
+public class budgetManager extends Budget {
+
+    public budgetManager(double initialBudget, double currentBudget) {
+        super(initialBudget, currentBudget);
+    }
+
+    public void adjustMonthlyBudget(double newBudgetSize) {
+        if (newBudgetSize > initialBudget) {
+            initialBudget = newBudgetSize;
+            double increasingDifference = newBudgetSize - initialBudget;
+            currentBudget = currentBudget + increasingDifference;
+        }
+        if (newBudgetSize == initialBudget) {
+            throw new IllegalArgumentException("Budget is the same");
+        }
+        else {
+            initialBudget = newBudgetSize;
+            double decreasingDifference = initialBudget - newBudgetSize;
+            if (currentBudget > decreasingDifference) {
+                currentBudget = currentBudget - decreasingDifference;
+            } else
+                throw new IllegalArgumentException("You've exceeded your monthly limit");
+        }
+    }
+
+    public void orderedMealsBudget(){
+        currentBudget = currentBudget;
+    }
+
+}
