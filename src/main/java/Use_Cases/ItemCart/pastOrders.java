2ff3c99367f2bda4f94027d0b14f7160bafcf55a
Aviral Bhardwaj <aviral.bhardwaj@mail.utoronto.ca>
2022-11-18 15:54:42 -0500

    Resolve conflict in pastOrder

--- src/main/java/Use_Cases/ItemCart/pastOrders.java
+++ src/main/java/Use_Cases/ItemCart/pastOrders.java
@@ -8,13 +8,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class pastOrders {
-<<<<<<< HEAD:src/main/java/Use_Cases/pastOrders.java
     private HashMap<String, Order> pastOrdersMap;
     private String lastOrdered;
-=======
-    protected HashMap<LocalDateTime, Order> pastOrders;
-    protected LocalDateTime lastOrdered;
->>>>>>> ba837d9ef596f639fc25b433f4ea0f634f33bd10:src/main/java/Use_Cases/ItemCart/pastOrders.java
 
     public pastOrders () {
         this.pastOrdersMap = new HashMap<String, Order>();
@@ -39,36 +34,20 @@ public class pastOrders {
     }
 
     public void addOrder (Order order) {
-<<<<<<< HEAD:src/main/java/Use_Cases/pastOrders.java
-        this.pastOrdersMap.put(order.dateOrdered.toString(), order);
+        this.pastOrdersMap.put(order.dateOrdered, order);
         this.lastOrdered = order.dateOrdered;
-=======
-        this.pastOrders.put(order.getDateOrdered(), order);
-        this.lastOrdered = order.getDateOrdered();
->>>>>>> ba837d9ef596f639fc25b433f4ea0f634f33bd10:src/main/java/Use_Cases/ItemCart/pastOrders.java
     }
 
     public double getTotalCost () {
         double cost = 0;
-<<<<<<< HEAD:src/main/java/Use_Cases/pastOrders.java
         for (Order order: this.pastOrdersMap.values()){
-            cost += order.totalCost;
-=======
-        for (Order order: this.pastOrders.values()){
             cost += order.getTotalCost();
->>>>>>> ba837d9ef596f639fc25b433f4ea0f634f33bd10:src/main/java/Use_Cases/ItemCart/pastOrders.java
         }
 
         return cost;
     }
 
-<<<<<<< HEAD:src/main/java/Use_Cases/pastOrders.java
-//    public double getCostOfLastOrdered () {
-//        return this.getOrderByDate(this.lastOrdered).totalCost;
-//    }
-=======
     public double getCostOfLastOrdered () {
         return this.getOrderByDate(this.lastOrdered).getTotalCost();
     }
->>>>>>> ba837d9ef596f639fc25b433f4ea0f634f33bd10:src/main/java/Use_Cases/ItemCart/pastOrders.java
 }
