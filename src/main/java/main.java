b7009d7d2313d4b139e51e9cf444c39779e255f0
Darpan Mishra <darpanmishra2003@gmail.com>
2022-11-18 22:05:06 -0500

    Created new RestaurantListingsFrame that utilizes Jswing and JTable to make a table to return listings of restaurants.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,12 @@
 import Frames.LoginFrame;
 import Frames.OnboardingFrame;
+import Frames.RestaurantListingsFrame;
 
 public class main{
 
     public static void main(String[] args) {
-        OnboardingFrame onboardingFrame = new OnboardingFrame();
+//        OnboardingFrame onboardingFrame = new OnboardingFrame();
+
+//        RestaurantListingsFrame restaurantListingsFrame = new RestaurantListingsFrame();
     }
 }
