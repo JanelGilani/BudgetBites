25d7809738668355a080d87d619d704b4470f215
Mann Thakkar <mann.thakkar@mail.utoronto.ca>
2022-11-20 13:40:36 -0500

    Added the RestaurantListingsFrame which displays the list of all restaurants

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,12 +1,11 @@
 import Frames.LoginFrame;
 import Frames.OnboardingFrame;
 import Frames.RestaurantListingsFrame;
+import Frames.SingleRestaurant;
 
 public class main{
 
     public static void main(String[] args) {
-//        OnboardingFrame onboardingFrame = new OnboardingFrame();
-
-//        RestaurantListingsFrame restaurantListingsFrame = new RestaurantListingsFrame();
+        OnboardingFrame onboardingFrame = new OnboardingFrame();
     }
 }
