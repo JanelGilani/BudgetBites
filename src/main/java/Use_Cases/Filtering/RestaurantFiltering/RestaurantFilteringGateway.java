3c0adbb3da6a7544ea58608d5e7dfaa5fb454bba
Qien Nathan Cai <qien.nathan@gmail.com>
2022-11-20 15:44:56 -0500

    Added methods for RestaurantFilteringGateway

--- src/main/java/Use_Cases/Filtering/RestaurantFiltering/RestaurantFilteringGateway.java
+++ src/main/java/Use_Cases/Filtering/RestaurantFiltering/RestaurantFilteringGateway.java
@@ -1,6 +1,28 @@
 package Use_Cases.Filtering.RestaurantFiltering;
+import Entities.Restaurant;
 import Use_Cases.mainMongoDB;
 
-public class RestaurantFilteringGateway {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class RestaurantFilteringGateway {
+    public static ArrayList<String> getListOfRestaurants() {
+        return null;
+    }
+
+    public static String getRestaurantCuisine(String name) {
+        return mainMongoDB.getRestaurantAttribute(name, "cuisine");
+    }
+
+    public static String getRestaurantPriceRange(String name) {
+        return mainMongoDB.getRestaurantAttribute(name, "priceRange");
+    }
+
+    public static String getRestaurantFoodType(String name) {
+        return mainMongoDB.getRestaurantAttribute(name, "foodType");
+    }
+
+    public static String getRestaurantAvgRating(String name) {
+        return mainMongoDB.getRestaurantAttribute(name, "avgRating");
+    }
 }
