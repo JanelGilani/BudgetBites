e76963784b940eb8b9e1e279dedda3b090988a6f
Qien Nathan Cai <qien.nathan@gmail.com>
2022-11-21 03:24:54 -0500

    Start RestaurantFilteringPresenter

--- src/main/java/Use_Cases/Filtering/RestaurantFiltering/RestaurantFilteringPresenter.java
+++ src/main/java/Use_Cases/Filtering/RestaurantFiltering/RestaurantFilteringPresenter.java
@@ -1,4 +1,9 @@
 package Use_Cases.Filtering.RestaurantFiltering;
+import java.util.ArrayList;
+
 
 public interface RestaurantFilteringPresenter {
+    ArrayList<String> fullList();
+
+    ArrayList<String> updateList();
 }
