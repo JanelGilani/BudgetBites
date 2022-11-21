5129ff1efdf244b96f07cdbd29ae46ad948983f8
vandanppatel <vandanp.patel@mail.utoronto.ca>
2022-11-21 15:14:01 -0500

    This is the login check. It will check if user exists and then will check if the inputted password is correct

--- src/main/java/Use_Cases/Login/logiccode.java
+++ src/main/java/Use_Cases/Login/logiccode.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.io.*;
 
 public class logiccode {
     public static void main(String[] args) {
