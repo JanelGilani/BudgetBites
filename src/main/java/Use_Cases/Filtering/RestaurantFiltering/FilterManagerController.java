3a670b1258c85210f8aabb4ea5256bb071e1b86f
Qien Nathan Cai <qien.nathan@gmail.com>
2022-11-21 03:23:03 -0500

    Initial Commit

---
+++ src/main/java/Use_Cases/Filtering/RestaurantFiltering/FilterManagerController.java
@@ -0,0 +1,10 @@
+package Use_Cases.Filtering.RestaurantFiltering;
+import Use_Cases.Filtering.RestaurantFiltering.RestaurantFilteringGateway;
+
+public class FilterManagerController {
+    private filterManager restaurantFilters;
+
+    public FilterManagerController() {
+    }
+
+}
