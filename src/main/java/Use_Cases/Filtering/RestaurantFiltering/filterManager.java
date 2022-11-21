ad3556ca80c1bbcf634f7034ad8952f3a0c0596f
Qien Nathan Cai <qien.nathan@gmail.com>
2022-11-21 15:33:31 -0500

    Change filtermanager to having hashmaps instead of having the restaurant filter class

--- src/main/java/Use_Cases/Filtering/RestaurantFiltering/filterManager.java
+++ src/main/java/Use_Cases/Filtering/RestaurantFiltering/filterManager.java
@@ -1,29 +1,28 @@
 package Use_Cases.Filtering.RestaurantFiltering;
 
-import Entities.Restaurant;
-import Use_Cases.Filtering.RestaurantFiltering.restaurantFilter;
-import Use_Cases.Filtering.RestaurantFiltering.RestaurantFilteringController;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class filterManager {
-    private restaurantFilter cuisineFilter;
+    private HashMap<String, ArrayList<String>> cuisineFilter;
 
-    private restaurantFilter foodTypeFilter;
+    private HashMap<String, ArrayList<String>> foodTypeFilter;
 
-    private restaurantFilter priceFilter;
+    private HashMap<String, ArrayList<String>> priceFilter;
 
-    public filterManager(restaurantFilter cuisineFilter, restaurantFilter foodTypeFilter, restaurantFilter priceFilter) {
+    public filterManager(HashMap<String, ArrayList<String>> cuisineFilter,
+                         HashMap<String, ArrayList<String>> foodTypeFilter,
+                         HashMap<String, ArrayList<String>> priceFilter) {
         this.cuisineFilter = cuisineFilter;
         this.foodTypeFilter = foodTypeFilter;
         this.priceFilter = priceFilter;
     }
 
     public ArrayList<String> filter(String pricePref, String cuisinePref, String foodTypePref) {
-        ArrayList<String> cuisineChoice = cuisineFilter.filter(cuisinePref);
-        ArrayList<String> foodTypeChoice = foodTypeFilter.filter(foodTypePref);
-        ArrayList<String> priceChoice = priceFilter.filter((pricePref));
+        ArrayList<String> cuisineChoice = cuisineFilter.getOrDefault(cuisinePref, null);
+        ArrayList<String> foodTypeChoice = foodTypeFilter.getOrDefault(foodTypePref, null);
+        ArrayList<String> priceChoice = priceFilter.getOrDefault(pricePref, null);
         if (!Objects.isNull(cuisineChoice)) {
             if (!Objects.isNull(foodTypeChoice)) {
                 cuisineChoice.retainAll(foodTypeChoice);
