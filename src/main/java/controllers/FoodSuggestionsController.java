3f59c0d90739128bcd54c2dc927bb87a0ae19717
Aviral Bhardwaj <aviral.bhardwaj@mail.utoronto.ca>
2022-12-07 16:38:03 -0500

    Added a foodSuggestion Controller
    Added a FoodSuggestion Controller

--- src/main/java/controllers/FoodSuggestionsController.java
+++ src/main/java/controllers/FoodSuggestionsController.java
@@ -1,4 +1,18 @@
 package controllers;
 
+import usecases.foodsuggestions.SuggestionToUser;
+
 public class FoodSuggestionsController {
+    private SuggestionToUser suggestionToUser;
+
+    public void setUseCase(SuggestionToUser s){
+        this.suggestionToUser = s;
+    }
+
+
+    public void callUseCase(String username) {
+//        System.out.println("line 11 controller");
+        suggestionToUser.toPresenter(username);
+    }
+
 }
