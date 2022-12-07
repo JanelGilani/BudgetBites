a636ffcc3c11786dd662d246f75866c3a463e73e
Aviral Bhardwaj <aviral.bhardwaj@mail.utoronto.ca>
2022-12-07 16:23:00 -0500

    Updated main.java
    Updated main java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,22 @@
-import ui.OnboardingFrame;
+import controllers.FoodSuggestionsController;
+import gateways.MainMongoDB;
+import presenters.FoodSuggestionPresenter;
 import ui.userSuggestionFrame;
+import usecases.SuggestionToUserDAI;
+import usecases.foodsuggestions.SuggestionToUser;
 
 public class main{
 
     public static void main(String[] args) {
-        OnboardingFrame onboardingFrame = new OnboardingFrame();
+        SuggestionToUserDAI dataBase = new MainMongoDB();
+        FoodSuggestionPresenter presenter = new FoodSuggestionPresenter();
+        SuggestionToUser suggestionToUser = new SuggestionToUser(dataBase);
+        FoodSuggestionsController controller = new FoodSuggestionsController();
+        userSuggestionFrame test = new userSuggestionFrame();
+
+        controller.setUseCase(suggestionToUser);
+        suggestionToUser.setPresenter(presenter);
+        presenter.setViewer(test);
+        test.setFoodSuggestionsController(controller);
     }
 }
