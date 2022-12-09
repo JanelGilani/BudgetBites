8135b5e501c46ef43a6fcac5870446ea40b3ee92
Qien Nathan Cai <qien.nathan@gmail.com>
2022-12-08 23:44:09 -0500

    Create and implement BudgetingController

--- src/main/java/controllers/BudgetingController.java
+++ src/main/java/controllers/BudgetingController.java
@@ -1,4 +1,29 @@
 package controllers;
 
+import gateways.MainMongoDB;
+import usecases.BudgetDAI;
+import usecases.budgeting.BudgetingInteractor;
+import entities.Budget;
+
+
 public class BudgetingController {
+    private final BudgetDAI budgetDAI = new MainMongoDB();
+
+    public BudgetingController() {
+    }
+
+    public int changeBudget(double budget, double confirmBudget, String user) {
+        if (!budgetDAI.userExists(user)) {
+            return 0;
+        } else if (BudgetingInteractor.newBudgetConfirm(budget, confirmBudget)) {
+            Budget userBudget = budgetDAI.getUserBudget(user);
+            BudgetingInteractor.adjustMonthlyBudget(userBudget, budget);
+            budgetDAI.updateAttributeByUsername(user, "budget", userBudget);
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+
 }
