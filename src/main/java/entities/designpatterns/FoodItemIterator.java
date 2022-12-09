b7eca1ce7e2c67a39745419fdeaf7525fa25db6e
aryangoel24 <aryangoel24@gmail.com>
2022-12-08 23:54:33 -0500

    Added documentation for design patterns

--- src/main/java/entities/designpatterns/FoodItemIterator.java
+++ src/main/java/entities/designpatterns/FoodItemIterator.java
@@ -3,8 +3,17 @@ package entities.designpatterns;
 import entities.FoodItem;
 
 public interface FoodItemIterator {
-    public boolean hasNext ();
-    public FoodItem next ();
+    /**
+     * Returns whether there is another element in the list
+     * @return True if there is another element in the list, False otherwise
+     */
+    boolean hasNext ();
+
+    /**
+     * Returns the next FoodItem
+     * @return Returns a FoodItem which is representative of the next FoodItem in a given list
+     */
+    FoodItem next ();
 
 
 }
--- src/main/java/entities/designpatterns/RestaurantFactory.java
+++ src/main/java/entities/designpatterns/RestaurantFactory.java
@@ -6,6 +6,15 @@ import entities.Restaurant;
 import java.util.ArrayList;
 
 public class RestaurantFactory {
+    /**
+     * Creates and returns a new restaurant with all the given parameters
+     * @param restaurantName: name of the restaurant
+     * @param priceRange: price range of the restaurant
+     * @param cuisine: cuisine of the restaurant
+     * @param foodType: food type of the restaurant
+     * @param avgRating: average rating of the restaurant
+     * @param menu: the menu of the restaurant
+     */
     public static Restaurant getRestaurant(String restaurantName, String priceRange, String cuisine,
                                            String foodType, double avgRating, ArrayList<FoodItem> menu) {
         return new Restaurant(restaurantName, priceRange, cuisine, foodType, avgRating, menu);
